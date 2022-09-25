bc28b4aa4acec568634d59d37909e23cb2622867
Qiwenweiii <Qiwenweiii@foxmail.com>
2022-09-25 23:24:41 +0800

    新增导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
     <RouterView></RouterView>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import SiteNavigation from './components/SiteNavigation.vue';
+</script>
 
 <style lang="scss" scoped></style>

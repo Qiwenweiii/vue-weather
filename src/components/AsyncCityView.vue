c1fc7b6823ebc623e758a35ca8e714da5ec0c45d
Qiwenweiii <Qiwenweiii@foxmail.com>
2022-09-26 23:17:40 +0800

    替换weatherAPIKey

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -3,25 +3,25 @@
 </template>
 
 <script setup>
-import axios from 'axios';
-import { useRoute } from 'vue-router';
+  import axios from 'axios';
+  import { useRoute } from 'vue-router';
 
-const route = useRoute();
-const weatherAPIKey = 'f2bfaf5880bea10526bbd6a1d3815e02';
+  const route = useRoute();
+  const weatherAPIKey = '7efa332cf48aeb9d2d391a51027f1a71';
 
-const { lat, lng } = route.query;
+  const { lat, lng } = route.query;
 
-const getWeatherData = async () => {
-  try {
-    const weatherData = await axios.get(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lng}&exclude={part}&appid=${weatherAPIKey}&lang=zh_cn`
-    );
-    return weatherData;
-  } catch (error) {
-    console.log(error);
-  }
-};
+  const getWeatherData = async () => {
+    try {
+      const weatherData = await axios.get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude={part}&appid=${weatherAPIKey}&lang=zh_cn`
+      );
+      return weatherData;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-const weatherData = await getWeatherData();
-console.log(weatherData);
+  const weatherData = await getWeatherData();
+  console.log(weatherData);
 </script>

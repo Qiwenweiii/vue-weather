cfefc778250aa697cf0dec1c844dbf5fed2337a2
Qiwenweiii <Qiwenweiii@foxmail.com>
2022-09-26 17:46:27 +0800

    新增天气请求，api key未激活，暂时未完成

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const weatherAPIKey = 'f2bfaf5880bea10526bbd6a1d3815e02';
+
+const { lat, lng } = route.query;
+
+const getWeatherData = async () => {
+  try {
+    const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lng}&exclude={part}&appid=${weatherAPIKey}&lang=zh_cn`
+    );
+    return weatherData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const weatherData = await getWeatherData();
+console.log(weatherData);
+</script>

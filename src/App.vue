05a02b1ac7b51ba2d98c294ad4d2a84642ceeaa0
Qiwenweiii <Qiwenweiii@foxmail.com>
2023-07-06 21:12:27 +0800

    添加顶部导航组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import SiteNavigation from './components/SiteNavigation.vue';
 </script>
 
 <template>
-  <RouterView />
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
+    <RouterView />
+  </div>
 </template>
 
---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">The Local Weather</p>
+        </div>
+      </RouterLink>
+
+      <div class="flex gap-3 flex-1 justify-end text-xl">
+        <i class="fa-solid fa-circle-info cursor-pointer hover:text-weather-secondary duration-300"></i>
+        <i class="fa-solid fa-plus cursor-pointer hover:text-weather-secondary duration-300"></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<style lang="scss" scoped>
+</style>

1e4420cd0aae58d15faeebc90a187843d54fcf2a
Qiwenweiii <Qiwenweiii@foxmail.com>
2023-07-09 17:46:07 +0800

    切换路由时更改document.title

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,31 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: '天气应用',
+      },
     },
     {
       path: '/weather/:state/:city/:area',
       name: 'city',
       component: () => import('../views/CityView.vue'),
+      meta: {
+        title: '城市天气',
+      },
     },
   ],
+});
+
+router.beforeEach((to, from) => {
+  const { state, city } = to.params;
+
+  let title = to.meta.title;
+
+  if (to.params.state) {
+    title = `${state}·${city}`;
+  }
+
+  document.title = `${title} | The Local Weather`;
 });
 
 export default router;

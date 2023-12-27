4eb8bd736b35825594d437cd93d5f2bf9df6dfdc
Qiwenweiii <Qiwenweiii@foxmail.com>
2023-12-27 18:20:02 +0800

    将请求的key移到config

--- src/hooks/useCity.js
+++ src/hooks/useCity.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { API_KEY } from '../../config';
 
 // location 可以是个城市名字符串，也可以是经纬度
-export const useCity = async (location) => {
-  const cityData = await axios.get(`https://geoapi.qweather.com/v2/city/lookup?location=${location}&key=d63bf1dc11fb44e6b78dfc81d64165b0`);
+export async function useCity(location) {
+  const cityData = await axios.get(
+    `https://geoapi.qweather.com/v2/city/lookup?location=${location}&key=${API_KEY}`
+  );
 
   return cityData.data;
-};
+}
--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { API_KEY } from '../../config';
 
 // location 一般为城市 id，type 为需要请求的天气类型，当前天气：'now'，24小时天气：'24h'，未来一周天气：'7d'
-export const useWeather = async (location, type) => {
-  const weatherData = await axios.get(`https://devapi.qweather.com/v7/weather/${type}?key=d63bf1dc11fb44e6b78dfc81d64165b0&location=${location}`);
+export async function useWeather(location, type) {
+  const weatherData = await axios.get(
+    `https://devapi.qweather.com/v7/weather/${type}?key=${API_KEY}&location=${location}`
+  );
 
   return weatherData.data;
-};
+}
